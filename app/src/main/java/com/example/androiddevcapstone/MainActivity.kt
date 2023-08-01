06ae09b44bd9879f90e98b40b5aa2ae073dde88e
unknown <martinnavratilu@gmail.com>
2023-08-01 15:07:33 +0200

    Onboarding+images

--- app/src/main/java/com/example/androiddevcapstone/MainActivity.kt
+++ app/src/main/java/com/example/androiddevcapstone/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevcapstone.ui.theme.AndroidDevCapstoneTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +39,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/androiddevcapstone/ui/theme/onboarding/Onboarding.kt
@@ -0,0 +1,15 @@
+package com.example.androiddevcapstone.ui.theme.onboarding
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Onboarding(){
+
+}
+
+@Preview
+@Composable
+fun OnboardingPreview(){
+    Onboarding()
+}
