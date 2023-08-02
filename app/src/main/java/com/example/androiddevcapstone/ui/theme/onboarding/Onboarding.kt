604d99249ba3f7e52dd575e865ced221d21c0cb9
MartinNav <martinnavratilu@gmail.com>
2023-08-02 20:48:40 +0200

    Added email regex

--- app/src/main/java/com/example/androiddevcapstone/ui/theme/onboarding/Onboarding.kt
+++ app/src/main/java/com/example/androiddevcapstone/ui/theme/onboarding/Onboarding.kt
@@ -90,8 +90,8 @@ Column {
         Row (modifier = Modifier.padding(15.dp,80.dp,15.dp,0.dp)){
             val context = LocalContext.current
             Button(onClick = {
-                if (uname==""||lname==""||email==""){
-                    Toast.makeText(context, "Please fill in all info", Toast.LENGTH_SHORT).show()
+                if (uname==""||lname==""|| !CheckEmailAddress(email)){
+                    Toast.makeText(context, "Please fill in correct info", Toast.LENGTH_SHORT).show()
                 }else {
                     //TODO:save login state using shared preferences
                     navController.navigate(ProfileD.route)
@@ -103,6 +103,11 @@ Column {
     }
 
 }
+}
+
+fun CheckEmailAddress(mail:String) : Boolean{
+    val reg = Regex("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    return reg.matches(mail)
 }
 /*
 @Preview
